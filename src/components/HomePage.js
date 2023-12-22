d814cfd0e27ef35fca3c8401a96ba98c39235ead
omar reda <oralshinnawy0@gmail.com>
2023-12-22 17:21:11 +0200

    intial code

---
+++ src/components/HomePage.js
@@ -0,0 +1,92 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+<div className="container">
+        <div className='Nav'>
+            <ul class="nav justify-content-center">
+                
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+</div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* <div className="container">
+    <div className="menu">
+                <img className="background-4" alt="Background" src="../assets/Rectangle 97.png" />
+        <div className="menu-2">
+                <div className="menu-wrapper">
+                    <div className="menu-3">
+                        <div className="overlap-group-4">
+                            <div className="line-2" />
+                                <div className="menu-4">
+                                    <div className="text-wrapper-26">Home</div>
+                                    <div className="about-2">
+                                        <img className="line-3" alt="Line" src="line-1.svg" />
+                                        <About className="about-instance" />
+                                    </div>
+                                    <div className="support-2">
+                                        <img className="line-3" alt="Line" src="line-3.svg" />
+                                        <Support className="support-instance" />
+                                    </div>
+                                    <div className="community-2">
+                                        <img className="line-3" alt="Line" src="line-2.svg" />
+                                        <Community className="community-instance" />
+                                    </div>
+                                    <div className="download-2">
+                                        <img className="line-4" alt="Line" src="line-4.svg" />
+                                        <Download className="download-instance" />
+                                    </div>
+                                    <div className="contact-US">
+                                        <img className="line-5" alt="Line" src="image.svg" />
+                                        <ContactUs className="contact-us-instance" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    </div>
+</div> */
+
+
